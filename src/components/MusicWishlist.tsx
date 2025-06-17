b5e3ba493b86770b84458a22bb62c04d786374c6
Syq <mauro.morelli@outlook.de>
2025-06-17 22:54:37 +0200

    Fix undefined checkConnectionStatus function

--- src/components/MusicWishlist.tsx
+++ src/components/MusicWishlist.tsx
@@ -140,7 +140,7 @@ export const MusicWishlist: React.FC<MusicWishlistProps> = ({ isDarkMode }) => {
       }
     };
     
-    checkConnectionStatus();
+    checkSpotify();
   }, []);
 
   // Search with debounce
